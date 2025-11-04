660fa9ce41361b0a1a17a1d19019015858dc39b8
Juan <juanlowandy@gmail.com>
2025-11-04 19:01:28 +0700

    Item class

--- src/week8_juan_117229_tutorial/BankTransferPayment.java
+++ src/week8_juan_117229_tutorial/BankTransferPayment.java
@@ -1,5 +1,15 @@
 package week8_juan_117229_tutorial;
 
-public class BankTransferPayment {
+public class BankTransferPayment extends Payment{
+	private String bankAccount;
+	
+	public BankTransferPayment(double amount, String bankAccount) {
+		super(amount);
+		this.bankAccount = bankAccount;
+	}
 
+	@Override
+	void processPayment() {
+		System.out.println("Processing bank transfer payment of $" + amount + "for bank account " + bankAccount);
+	}
 }
--- src/week8_juan_117229_tutorial/CreditCardPayment.java
+++ src/week8_juan_117229_tutorial/CreditCardPayment.java
@@ -1,5 +1,15 @@
 package week8_juan_117229_tutorial;
 
-public class CreditCardPayment {
+public class CreditCardPayment extends Payment{
+	private String cardNumber;
+	
+	public CreditCardPayment(double amount, String cardNumber) {
+		super(amount);
+		this.cardNumber = cardNumber;
+	}
 
+	@Override
+	void processPayment() {
+		System.out.println("Processing credit card payment of $" + amount + "for card number " + cardNumber);
+	}
 }
